8a4f131d188356ffa0c0586f3085f90e722ec084
Fernando javier Fernandez <yofercho@gmail.com>
2019-02-28 07:44:21 -0500

    Noise Reduction

--- Labs/js/filters.js
+++ Labs/js/filters.js
@@ -26,19 +26,44 @@ function Averaging(){
 function Bluring(){
     //Removes Noise of the image
     //canvas output
-    var out=document.getElementById("imShow");
-    var imSrc=cv.imread(img1);
-    //Imagen destino
+    let src = cv.imread(img1);
     let dst = new cv.Mat();
-    //Creamos un kernel de 3X3
-    let M = cv.Mat.ones(5, 5, cv.CV_32F);
-    //Anchor Centro del Kernel
+    let ksize = new cv.Size(5, 5);
     let anchor = new cv.Point(-1, -1);
-    //Filter byass 0
-    cv.filter2D(imSrc, dst, cv.CV_8U, M, anchor, 0, cv.BORDER_DEFAULT);
-    cv.imshow(out, dst);
+    // You can try more different parameters
+    cv.blur(src, dst, ksize, anchor, cv.BORDER_DEFAULT);
+    // cv.boxFilter(src, dst, -1, ksize, anchor, true, cv.BORDER_DEFAULT)
+    cv.imshow('imShow', dst);
+    src.delete(); dst.delete();
 
+}
 
+function GaussianBlur(){
+    let src = cv.imread(img1);
+    let dst = new cv.Mat();
+    let ksize = new cv.Size(5, 5);
+    // You can try more different parameters
+    cv.GaussianBlur(src, dst, ksize, 0, 0, cv.BORDER_DEFAULT);
+    cv.imshow('imShow', dst);
+    src.delete(); dst.delete();
+}
+function MedianBlur(){
+    let src = cv.imread(img1);
+    let dst = new cv.Mat();
+    // You can try more different parameters
+    cv.medianBlur(src, dst, 5);
+    cv.imshow('imShow', dst);
+    src.delete(); dst.delete();
+}
+
+function BilateralFilter(){
+    let src = cv.imread(img1);
+    let dst = new cv.Mat();
+    cv.cvtColor(src, src, cv.COLOR_RGBA2RGB, 0);
+    // You can try more different parameters
+    cv.bilateralFilter(src, dst, 15, 80, 80, cv.BORDER_DEFAULT);
+    cv.imshow('imShow', dst);
+    src.delete(); dst.delete();
 }
 
 function ApplyFilter(){
@@ -52,10 +77,13 @@ function ApplyFilter(){
             Bluring();
             break;
         case "GBlur":
+            GaussianBlur();
             break;
-        case "MBlure":
+        case "MBlur":
+            MedianBlur();
             break;
         case "BFilter":
+            BilateralFilter();
             break;
         default:
             break;
